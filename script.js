258a23ba8a22e85cb82e9df39c20caf7d8e2e180
Matej Bartoncik <matejbartoncik@seznam.cz>
2023-10-12 21:21:10 +0200

    added script to match diff level with number of pairs

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('onload', function ()  {
+        for (let i = 0; i < difficulty.length; i++) {
+        difficulty[i].checked = false;
+}});
+const startButton = document.getElementById("start-button")
+const difficulty = document.getElementsByName("difficulty");
+function pairsMatchDiff() {
+    let pairs = 0;
+    if (difficulty[0].checked) {
+         pairs =8; 
+    } else if(difficulty[1].checked) {
+        pairs =16;
+    }else if(difficulty[2].checked){
+        pairs = 24;
+    }  
+};
+
