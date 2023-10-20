eee69c00c378b652b3efe521cf021631795e4b2f
Matej Bartoncik <matejbartoncik@seznam.cz>
2023-10-20 14:11:11 +0200

    some desing + js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ window.addEventListener('onload', function ()  {
 
 const startButton = document.getElementById("start-button")
 const difficulty = document.getElementsByName("difficulty");
+let pairs = 0;
 function pairsMatchDiff() {
-    let pairs = 0;
     if (difficulty[0].checked) {
          pairs =8; 
     } else if(difficulty[1].checked) {
@@ -18,6 +18,37 @@ function pairsMatchDiff() {
 };
 function shuffle() {
     
+}
+
+function changeBackground() {
+    const label = document.getElementsByName("label");
+    const checked0 = difficulty[0].checked;
+    const checked1 = difficulty[1].checked;
+    const checked2 = difficulty[2].checked;
+
+    switch (true) {
+        case checked0:
+            label[0].style.color = "white";
+            label[1].style.color = "black";
+            label[2].style.color = "black";
+            break;
+
+        case checked1 :
+            label[0].style.color = "black";
+            label[1].style.color = "white";
+            label[2].style.color = "black";
+            break;
+
+        case checked2 :
+            label[0].style.color = "black";
+            label[1].style.color = "black";
+            label[2].style.color = "white";
+            break;
+
+        default:
+            // Default case if none of the conditions are met
+            break;
+    }
 }
 
 
