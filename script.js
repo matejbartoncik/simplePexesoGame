bc3822afb0292cae8a1f5e9038b673ab997f1714
Matej Bartoncik <matejbartoncik@seznam.cz>
2023-10-24 11:04:56 +0200

    added few styling things and js

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
-window.addEventListener('onload', function ()  {
-        for (let i = 0; i < difficulty.length; i++) {
-        difficulty[i].checked = false;
-}});
-
-
-const startButton = document.getElementById("start-button")
+const startButton = document.getElementById("start-button");
 const difficulty = document.getElementsByName("difficulty");
+const startSection = document.getElementsByName("startSection")
+const gameBoard = document.getElementsByName("gameBoard")
 let pairs = 0;
-function pairsMatchDiff() {
-    if (difficulty[0].checked) {
-         pairs =8; 
-    } else if(difficulty[1].checked) {
-        pairs =16;
-    }else if(difficulty[2].checked){
-        pairs = 24;
-    }  
-};
-function shuffle() {
-    
+window.addEventListener("onload", function () {
+  for (let i = 0; i < difficulty.length; i++) {
+    difficulty[i].checked = false;
+  }
+});
+
+function startGame() {
+    startSection[0].style.display="none"
+    gameBoard[0].style.display="block"
+    pairsMatchDiff()
 }
+function pairsMatchDiff() {
+  if (difficulty[0].checked) {
+    pairs = 8;
+  } else if (difficulty[1].checked) {
+    pairs = 16;
+  } else if (difficulty[2].checked) {
+    pairs = 24;
+  }
+}
+function shuffle() {}
 
 function changeBackground() {
-    const label = document.getElementsByName("label");
-    const checked0 = difficulty[0].checked;
-    const checked1 = difficulty[1].checked;
-    const checked2 = difficulty[2].checked;
+  const label = document.getElementsByName("label");
 
-    switch (true) {
-        case checked0:
-            label[0].style.color = "white";
-            label[1].style.color = "black";
-            label[2].style.color = "black";
-            break;
+  for (let i = 0; i < label.length; i++) {
+    const isChecked = difficulty[i].checked;
 
-        case checked1 :
-            label[0].style.color = "black";
-            label[1].style.color = "white";
-            label[2].style.color = "black";
-            break;
-
-        case checked2 :
-            label[0].style.color = "black";
-            label[1].style.color = "black";
-            label[2].style.color = "white";
-            break;
-
-        default:
-            // Default case if none of the conditions are met
-            break;
+    if (isChecked) {
+        label[i].classList.add("label-checked");
+        label[i].classList.remove("label-style");
+    } else {
+      label[i].classList.remove("label-checked");
+      label[i].classList.add("label-style");
     }
+  }
 }
-
-
