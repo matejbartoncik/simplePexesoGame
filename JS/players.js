7ce49c8039657257bc11c40938b215f019b40410
MistrMatej <matejbartoncik@seznam.cz>
2023-12-03 00:25:58 +0100

    zacatek pridavani js pro hrace

--- JS/players.js
+++ JS/players.js
@@ -1,5 +1,16 @@
+const playerScore1 = document.getElementById("player1Score");
+const player2Score = document.getElementById("player2Score");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+
+function addScore() {
+    if (player1.innerHTML) {
+      
+    }
+}
+
+//tato funkce je nefunkční
 function playerSwitch() {
-  activeCardsID = [];
   if (PLAYER_ONE == true) {
     PLAYER_ONE = false;
     PLAYER_TWO = true;
