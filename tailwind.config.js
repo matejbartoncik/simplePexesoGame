77daa269ea92c4d1b5ce83e45f604c5c88bca6d7
MistrMatej <matejbartoncik@seznam.cz>
2023-12-16 18:13:44 +0100

    added some stuff - not working ;)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const { postcss } = require('tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode="jit",
+  purge: {
+    enabled: true,
+    content: ["./**/*.{html,js}",
+    "/JS/**/*..js"],
+  },
   content: ["./**/*.{html,js}",
-              "/JS/**/*..js"
-    ],
+  "/JS/**/*..js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [postcss],
 }
+
