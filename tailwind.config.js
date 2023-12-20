40c74990940a10b59fa4332b21a96427da2a40c3
Matej Bartoncik <matejbartoncik@seznam.cz>
2023-12-20 11:41:37 +0100

    Delete unused images and update CSS and HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
   "/JS/**/*..js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-bg': "url('/images/pexesoBG.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
+    },
   },
   variants: {
     extend: {},
